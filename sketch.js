b4d6a17ea33b642f49fa17564c8ad9086438f4a6
Anika-glitch <72518105+anikapravin@users.noreply.github.com>
2023-08-31 12:33:49 +0530

    aug31
    aug 31st

--- sketch.js
+++ sketch.js
@@ -1,46 +1,51 @@
-
 function windowResized(){
-    resizeCanvas(windowWidth,windowHeight/2);
-  }
-
-  function setup() {
-      let m;
-  let x = 0;
-  var canvas;
-   canvas= createCanvas(windowWidth, windowHeight, WEBGL);
-  canvas.position(0,0);
-  canvas.style('z-index','-1');
-  }
-  
-  
-  
-  function draw() {
-      if (windowWidth>800)
-      {   background(255,255,255,0);
-
-  let x=255;
-  
-  
-    let i=1;
-  
-    
-
-   // translate(p5.Vector.fromAngle(millis() / 1000, 10));
-    for(let i=1;i<3;i++){
-      
-     noFill();
-      stroke(226,94,250);
-  
-    torus(1,windowHeight*2,5);
-  
-     
-      rotateX(frameCount/1000);  
-      rotateY(frameCount/1000);
-  
-    }
-    
-    }
-   
+  resizeCanvas(windowWidth,windowHeight/2);
 }
+
+
+
+function setup() {
+    let m;
+let x = 0;
+var canvas;
+ canvas= createCanvas(windowWidth/3, windowHeight, WEBGL);
+canvas.position(0,0);
+canvas.style('z-index','-1');
+
+}
+
+
+
+function draw() {
+
+    if (windowWidth>800)
+    {   background(255,255,255,0);
+
+let x=255;
+
+
+  let i=1;
+
   
+ // translate(p5.Vector.fromAngle(millis() / 1000, 10));
+
+  for(let i=1;i<3;i++){
+ 
+    
+   noFill();
+    //stroke(226,94,250);
+    stroke(96,227,247);
+  torus(mouseX,windowHeight,5);
+text ("HI",0,0);
+   
+    rotateX(frameCount/1000);  
+    rotateY(frameCount/1000);
+ 
+  }
+
+  }
+
+}
+
+
   
